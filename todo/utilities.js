f4fdddeecf57de5f084aa4a1e5d7575cce5f5368
Briana Olsen <bria.olsen@gmail.com>
2020-10-20 23:50:40 -0600

    week6-4

--- todo/utilities.js
+++ todo/utilities.js
@@ -1,6 +1,6 @@
 import ToDos from './ToDos.js'
 
-const tasks = new ToDos('todos-div');
+const tasks = new ToDos('todos-div', 'add-btn', 'task-input');
 
 window.addEventListener('load', () => {
   tasks.showAll();
